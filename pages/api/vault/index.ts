380220f21c24398fecd0edbe312b7df1198ab6bf
Karlo Bertina <kerlexov@gmail.com>
2022-12-08 18:13:19 +0100

    vault client fix

--- pages/api/vault/index.ts
+++ pages/api/vault/index.ts
@@ -5,12 +5,6 @@ const handler = (req:any, res:any) => {
     if (req.method === 'POST') {
     //name,author,encFlag,
         const dec = decryptVault({vaultKey:req.body.encFlag,vault: hashData(req.body.name+"_"+sec+"_"+req.body.author)})
-        vaultClient.approleLogin({
-            role_id: "47403516-81b1-72ab-2d9c-338104463d2b",
-            secret_id: "891b8f30-ee1a-2a45-df7e-d771b4e82f80",
-        }).then((result:any)=>{
-            vaultClient.token = result.auth.client_token;
-        });
 
         vaultClient.write("secret/ctf/"+hashData(req.body.name), {encFlag: req.body.encFlag, flag: dec}).then((r:any) =>{
             console.log("ovo je vault response")
