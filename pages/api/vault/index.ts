63539c9f6cee23da3aaf8f58fc5f6733f454893f
Karlo Bertina <kerlexov@gmail.com>
2022-12-08 17:15:09 +0100

    vault change path

--- pages/api/vault/index.ts
+++ pages/api/vault/index.ts
@@ -1,18 +1,17 @@
 import {decryptVault, hashData, sec} from "../../../src/utility";
-import readVault from "../../../src/utility/vaultClient";
 import vaultClient from "../../../src/utility/vaultClient";
 
 const handler = (req:any, res:any) => {
     if (req.method === 'POST') {
     //name,author,encFlag,
         const dec = decryptVault({vaultKey:req.body.encFlag,vault: hashData(req.body.name+"_"+sec+"_"+req.body.author)})
-        vaultClient.write(hashData(req.body.name), JSON.stringify({encFlag: req.body.encFlag, flag: dec})).then((r:any) =>{
+        vaultClient.write("secret/ctf/"+hashData(req.body.name), JSON.stringify({encFlag: req.body.encFlag, flag: dec})).then((r:any) =>{
             console.log("ovo je vault response")
             console.log(r)
+            res.status(200).json({ data: r})
         }).catch((e:any)=>{
             res.status(500).json({err:e})
         })
-        res.status(200).json({ data: req.body})
 
         // const vval = readVault("secret/data/ctf/app")
         //console.log(vval)
