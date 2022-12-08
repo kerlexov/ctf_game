bbdac3af4dd13ec021ecc64062a602aee1eaab04
Karlo Bertina <kerlexov@gmail.com>
2022-12-08 17:48:18 +0100

    vault corsfix

--- src/utility/vaultClient.ts
+++ src/utility/vaultClient.ts
@@ -7,12 +7,4 @@ var options = {
 const vaultClient = require("node-vault")(options);
 //       "Command": "server -config=/vault/config/vault-config.json"
 
-
-vaultClient.approleLogin({
-    role_id: "47403516-81b1-72ab-2d9c-338104463d2b",
-    secret_id: "891b8f30-ee1a-2a45-df7e-d771b4e82f80",
-}).then((result:any)=>{
-    vaultClient.token = result.auth.client_token;
-});
-
 export default vaultClient;
