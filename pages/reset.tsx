9c0709b963d8e6402092b88ae66855643a193c35
Karlo Bertina <kerlexov@gmail.com>
2023-01-19 03:06:35 +0100

    profile change password forgot password reset page

--- pages/reset.tsx
+++ pages/reset.tsx
@@ -28,48 +28,48 @@ export const ResetPage: React.FC = (props, context) => {
 
     const [changeValue,setChangeValue] = useState("")
     return <>
-        <Row style={{height:"100vh"}}>
-            <Col span={12} >
-                <Card
-                    hoverable
-                    style={{ width: "100%", height: "100%",backgroundColor: "#3d2442", color: "#fff" }}
-                >
-                    <Form style={{marginTop:"3em",width: "80%"}}>
-                        <Title style={{color:"white"}} level={5}>Password change</Title>
-                        <Input minLength={8} maxLength={128} required type={"password"}  onChange={(c)=>{
-                            setChangeValue(c.target.value)
-                        }} placeholder="********" />
+        {u&&s?(<Row style={{height:"100vh"}}>
+        <Col span={12} >
+            <Card
+                hoverable
+                style={{ width: "100%", height: "100%",backgroundColor: "#3d2442", color: "#fff" }}
+            >
+                <Form style={{marginTop:"3em",width: "80%"}}>
+                    <Title style={{color:"white"}} level={5}>Password change</Title>
+                    <Input minLength={8} maxLength={128} required type={"password"}  onChange={(c)=>{
+                        setChangeValue(c.target.value)
+                    }} placeholder="********" />
 
-                        <Button style={{marginTop:"2em"}} onClick={async (c) => {
-                            if (changeValue.length > 8) {
-                                const promise = await account.updateRecovery(u, s, hashData(changeValue), hashData(changeValue))
-                                if(promise){
-                                    notificationProvider.open({
-                                        message: "Password changed",
-                                        key: "pchageea",
-                                        type: "error"
-                                    })
-                                }else {
-                                    notificationProvider.open({
-                                        message: "Failed to change password",
-                                        key: "pchageeas",
-                                        type: "error"
-                                    })
-                                }
-                                setChangeValue("")
-                            } else {
+                    <Button style={{marginTop:"2em"}} onClick={async (c) => {
+                        if (changeValue.length > 8) {
+                            const promise = await account.updateRecovery(u, s, hashData(changeValue), hashData(changeValue))
+                            if(promise){
                                 notificationProvider.open({
-                                    message: "Password must be at least 8 characters",
+                                    message: "Password changed",
                                     key: "pchageea",
                                     type: "error"
                                 })
+                            }else {
+                                notificationProvider.open({
+                                    message: "Failed to change password",
+                                    key: "pchageeas",
+                                    type: "error"
+                                })
                             }
+                            setChangeValue("")
+                        } else {
+                            notificationProvider.open({
+                                message: "Password must be at least 8 characters",
+                                key: "pchageea",
+                                type: "error"
+                            })
                         }
-                        } type={"primary"}>Change password</Button>
-                    </Form>
-                </Card>
-            </Col>
-        </Row>
+                    }
+                    } type={"primary"}>Change password</Button>
+                </Form>
+            </Card>
+        </Col>
+    </Row>):(<></>)}
      </>
 }
 export const getServerSideProps:  GetServerSideProps = async (context) => {
