8a506c6d2b69b16cdfd56d6bfb7d1f80071511c9
Karlo Bertina <kerlexov@gmail.com>
2023-01-19 03:03:58 +0100

    profile change password forgot password reset page

--- pages/reset.tsx
+++ pages/reset.tsx
@@ -4,6 +4,8 @@ import {LayoutWrapper, useUpdatePassword} from "@pankod/refine-core";
 
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 import {GetServerSideProps} from "next";
+import {useRouter} from "next/router";
+import {account, hashData} from "../src/utility";
 
 
 const { Title } = Typography;
@@ -12,16 +14,25 @@ type updatePasswordVariables = {
     password: string;
 };
 export const ResetPage: React.FC = (props, context) => {
-
-    console.log(context)
+    const router = useRouter()
+    const url1 = new URL(`https://ctf-game.vercel.app${router.query.to}`);
+    let u;let s;
+    url1.searchParams.forEach((value, key, parent)=>{
+        if(key === "userId"){
+            u = value;
+        }
+        if(key === "secret"){
+            s = value;
+        }
+    })
 
     const [changeValue,setChangeValue] = useState("")
     return <>
-        <Row>
+        <Row style={{height:"100vh"}}>
             <Col span={12} >
                 <Card
                     hoverable
-                    style={{ width: "100%", height: "33vh",backgroundColor: "#3d2442", color: "#fff" }}
+                    style={{ width: "100%", height: "100%",backgroundColor: "#3d2442", color: "#fff" }}
                 >
                     <Form style={{marginTop:"3em",width: "80%"}}>
                         <Title style={{color:"white"}} level={5}>Password change</Title>
@@ -29,30 +40,37 @@ export const ResetPage: React.FC = (props, context) => {
                             setChangeValue(c.target.value)
                         }} placeholder="********" />
 
-                        <Button style={{marginTop:"2em"}} onClick={(c)=>{
-                            if(changeValue.length>8){
-
+                        <Button style={{marginTop:"2em"}} onClick={async (c) => {
+                            if (changeValue.length > 8) {
+                                const promise = await account.updateRecovery(u, s, hashData(changeValue), hashData(changeValue))
+                                if(promise){
+                                    notificationProvider.open({
+                                        message: "Password changed",
+                                        key: "pchageea",
+                                        type: "error"
+                                    })
+                                }else {
+                                    notificationProvider.open({
+                                        message: "Failed to change password",
+                                        key: "pchageeas",
+                                        type: "error"
+                                    })
+                                }
                                 setChangeValue("")
-                            }else {
-                                notificationProvider.open({message:"Password must be at least 8 characters",key:"pchageea",type:"error"})
+                            } else {
+                                notificationProvider.open({
+                                    message: "Password must be at least 8 characters",
+                                    key: "pchageea",
+                                    type: "error"
+                                })
                             }
                         }
                         } type={"primary"}>Change password</Button>
                     </Form>
                 </Card>
             </Col>
-            <Col span={12} >
-
-            </Col>
         </Row>
-        <Row gutter={[2, 2]}>
-            <Col span={12} >
-
-            </Col>
-            <Col span={12} >
-
-            </Col>
-        </Row></>
+     </>
 }
 export const getServerSideProps:  GetServerSideProps = async (context) => {
     {
